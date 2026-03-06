9997a9fdcb52319419b8bc8c2ee5344f29a53d5b
jhewu <junyi.hewu2003@gmail.com>
2026-03-06 18:45:39 -0500

    fixed remaining bugs

--- subprocess.py
+++ subprocess.py
@@ -14,7 +14,7 @@ def run_subprocess():
     # Check if directories exist before running the command
     # ARGS = [ [LIST_0], [LIST_1], [LIST_2]]
     for i in range(K): 
-        in_dir = ARGS[i][2]
+        in_dir = ARGS[i][1]
         if os.path.exists(in_dir): 
             print("Directory exists: ", in_dir)
         else: 
@@ -25,8 +25,8 @@ def run_subprocess():
         print(f"Running fold {i+1}/{K}...")
 
         # Expand the command to include the parameter directory
-        fold_command = command.extend(ARGS[i])
-        result = subprocess.run(fold_command, text=True)
+        k_command = command + ARGS[i]
+        result = subprocess.run(k_command, text=True)
 
         # Print the result
         print("Output: ", result.stdout) # Output from the script
@@ -62,18 +62,19 @@ if __name__ == "__main__":
         # For Running 'yolo_crop.py'
         ARGS = [
             [
-            "--in_dir", '3_fold_dataset/stacked_segmentation_0' # UPDATE
-            "--model_dir", 'pretrained_yolo/best_0.pt' # UPDATE
+            "--in_dir", '3_fold_dataset/stacked_segmentation_0', # UPDATE
+            "--model_dir", 'pretrained_yolo/best_0.pt', # UPDATE
             "--device", 'cuda', 
             '--batch_size', '16',
             '--image_size', '160',
             '--confidence', '0.70',
             '--margin_of_error', '30',
             '--workers', '8',
+
             ],
             [
-            "--in_dir", '3_fold_dataset/stacked_segmentation_1' # UPDATE
-            "--model_dir", 'pretrained_yolo/best_1.pt' # UPDATE
+            "--in_dir", '3_fold_dataset/stacked_segmentation_1', # UPDATE
+            "--model_dir", 'pretrained_yolo/best_1.pt', # UPDATE
             "--device", 'cuda', 
             '--batch_size', '16',
             '--image_size', '160',
@@ -82,8 +83,8 @@ if __name__ == "__main__":
             '--workers', '8',
             ],
                         [
-            "--in_dir", '3_fold_dataset/stacked_segmentation_2' # UPDATE
-            "--model_dir", 'pretrained_yolo/best_2.pt' # UPDATE
+            "--in_dir", '3_fold_dataset/stacked_segmentation_2', # UPDATE
+            "--model_dir", 'pretrained_yolo/best_2.pt', # UPDATE
             "--device", 'cuda', 
             '--batch_size', '16',
             '--image_size', '160',
